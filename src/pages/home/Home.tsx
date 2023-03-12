ec8f0adf078aae7483b4cef240dfc5fb1db2834f
HocNguyenInnomize <nguyenthaihocxm1999@gmail.com>
2023-03-12 13:58:59 +0700

    Integrate to DB and update new UI

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useRef } from "react";
 import { Button, Carousel, ConfigProvider, Form, Input, Radio } from "antd";
 
@@ -10,28 +11,26 @@ import Spirit from "../../assets/images/spirit.jpg";
 import Journey3Year from "../../assets/images/journey-3-year.jpg";
 import Journey7Year from "../../assets/images/journey-7-year.jpg";
 import Currently from "../../assets/images/currently.jpg";
+import InvitationCard from "../../assets/images/invitation-card.jpg";
 import TN1 from "../../assets/images/tn1.jpg";
 import TN2 from "../../assets/images/tn2.jpg";
 import TN3 from "../../assets/images/tn3.jpg";
 import TN4 from "../../assets/images/tn4.jpg";
-import TN5 from "../../assets/images/tn5.jpg";
 import TN6 from "../../assets/images/tn6.jpg";
 import TN7 from "../../assets/images/tn7.jpg";
 import TN8 from "../../assets/images/tn8.jpg";
 import TN9 from "../../assets/images/tn9.jpg";
-import TN11 from "../../assets/images/tn10.jpg";
-import TN12 from "../../assets/images/tn11.jpg";
-import TN13 from "../../assets/images/tn12.jpg";
-import TN14 from "../../assets/images/tn13.jpg";
-import axios from "axios";
 
 import "./style.css";
 import { useProgressiveImage } from "../../hooks";
 
-const URL = "http://localhost:5001/api/user-controller/user";
+const URL =
+  "https://sheet.best/api/sheets/52f32189-fac3-4a04-adcc-f7f49c0a1c7e";
 
 export const Home = () => {
   const myRef: any = useRef(null);
+  const [form] = Form.useForm();
+
   const TNs: any[] = [TN1, TN2, TN3, TN4, TN6, TN7, TN8, TN9];
   const TN_images: any[] = TNs.map((tn) => (
     <div>
@@ -52,8 +51,23 @@ export const Home = () => {
     background: "#364d79",
   };
 
-  const handleSubmit = (data: any) => {
-    axios.post(URL, data);
+  const onFinish = (values: any) => {
+    axios
+      .post(URL, {
+        "Họ và tên": values?.fullName,
+        "Quyết định": values?.decision ? "Có nè" : "Bận mất rồi!",
+        "Số người đi cùng": values?.people,
+      })
+      .then((response) => {
+        form.resetFields();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
   };
 
   return (
@@ -234,9 +248,10 @@ export const Home = () => {
                     style={{
                       textAlign: "center",
                       textJustify: "inter-word",
+                      whiteSpace: "pre-wrap",
                     }}
                   >
-                    NHỮNG KHOẢNH KHẮC TRỌN VẸN CỦA CHÚNG MÌNH
+                    {`NHỮNG KHOẢNH KHẮC TRỌN VẸN\nCỦA CHÚNG MÌNH`}
                   </h3>
                 </div>
               </div>
@@ -272,6 +287,14 @@ export const Home = () => {
             </div>
           </div>
         </div>
+
+        <div id="SECTION314" className="ladi-section">
+          <div
+            className="ladi-section-background"
+            style={{ backgroundImage: `url(${InvitationCard})` }}
+          ></div>
+        </div>
+
         <div id="SECTION315" className="ladi-section">
           <div className="ladi-section-background"></div>
           <div className="ladi-overlay"></div>
@@ -284,29 +307,34 @@ export const Home = () => {
             </div>
             <div id="PARAGRAPH394" className="ladi-element">
               <div className="ladi-paragraph">
-                Hãy sắp xếp thời gian đến chung vui cùng Sắc Màu trong ngày Kỷ
-                niệm 10 năm đặc biệt này nhé! Chúng ta sẽ cùng nhau ôn lại những
-                kỷ niệm đẹp, thưởng thức teabreak nho nhỏ cùng nhau, xem lại
-                những đóng góp tuyệt vời đã cùng nhau thực hiện trong 1 thập kỷ
-                qua.
+                Chương trình kỉ niệm 10 năm hoạt động là sự kiện để chúng ta
+                cùng nhau lắng đọng nhìn lại hành trình sẻ chia trong cuộc sống,
+                cùng nhau chứng kiến những thành quả được tạo nên bởi tấm lòng
+                nhân ái, những giá trị mà các anh chị đã trao đi cho cộng đồng
+                thông qua Sắc Màu.
                 <br />
               </div>
             </div>
             <div id="PARAGRAPH395" className="ladi-element">
               <div className="ladi-paragraph">
-                Buổi họp mặt sẽ ý nghĩa trọn vẹn, đông đầy cảm cúc khi có sự
-                tham dự của Anh/Chị. Tất cả chúng ta sẽ cùng gặp nhau tại Buổi
-                Họp mặt Kỷ niệm 10 năm thành lập Tổ chức Xã hội Sắc Màu với chủ
-                đề: DẤU ẤN MỘT THẬP KỶ - VÌ CUỘC SỐNG TỐT ĐẸP HƠN” diễn ra tại
-                EDENSTAR SAIGON HOTEL - Số 38 Bùi Thị Xuân, P. Phạm Ngũ Lão,
-                Quận 1, TP. HCM.
+                Với vai trò là cầu nối và tổ chức, Sắc Màu vô cùng biết ơn
+                anh/chị đã hiện diện cùng Sắc Màu trong hành trình 10 năm Sắc
+                Màu Yêu Thương. Chính vì vậy, Sắc Màu rất mong được chào đón anh
+                chị quay về họp mặt, ôn lại những kỷ niệm đẹp, giao lưu chia sẻ
+                về chủ đề thiện nguyện cùng Sắc Màu, đặc biệt là thưởng thức
+                những tiết mục văn nghệ do chính các em sinh viên “nhà Sắc Màu”
+                biểu diễn.
                 <br />
               </div>
             </div>
             <div id="PARAGRAPH3951" className="ladi-element">
               <div className="ladi-paragraph">
-                Hãy xác nhận tham gia để Sắc Màu chuẩn bị cho “Ngày hội tụ” thật
-                chu đáo nhé!
+                Thông qua đây Sắc Màu cũng xin gửi lời cảm ơn sâu sắc đến các
+                chị trong Ban cố vấn/giám sát của Tổ Chức Xã hội Sắc Màu đã tài
+                trợ tiệc tea break cũng như toàn bộ chi phí tổ chức chương trình
+                kỉ niệm này. Để giúp BTC thực hiện tốt vai trò của mình trong
+                công tác tổ chức, anh chị vui lòng xác nhận tham dự trước ngày
+                15/03/2023.
                 <br />
               </div>
             </div>
@@ -316,12 +344,14 @@ export const Home = () => {
               className="ladi-element"
             >
               <Form
+                form={form}
                 name="basic"
                 labelCol={{ span: 8 }}
                 wrapperCol={{ span: 16 }}
                 className="ladi-form"
                 initialValues={{ remember: true }}
-                onFinish={handleSubmit}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
                 autoComplete="off"
               >
                 <div id="FORM_ITEM396" className="ladi-element">
@@ -342,58 +372,21 @@ export const Home = () => {
                   </Form.Item>
                 </div>
                 <div id="FORM_ITEM397" className="ladi-element">
-                  <Form.Item
-                    name="phoneNumber"
-                    rules={[
-                      {
-                        required: true,
-                        message: "",
+                  <ConfigProvider
+                    theme={{
+                      token: {
+                        colorPrimary: "#00b96b",
                       },
-                    ]}
+                    }}
                   >
-                    <Input
-                      type="number"
-                      size="large"
-                      style={{ width: "387px" }}
-                      placeholder="Số điện thoại"
-                    />
-                  </Form.Item>
-                </div>
-                <div id="FORM_ITEM3970" className="ladi-element">
-                  <Form.Item
-                    name="email"
-                    rules={[
-                      {
-                        type: "email",
-                        required: true,
-                        message: "",
-                      },
-                    ]}
-                  >
-                    <Input
-                      type="email"
-                      size="large"
-                      style={{ width: "387px" }}
-                      placeholder="Đại chỉ email "
-                    />
-                  </Form.Item>
-                </div>
-                <div id="FORM_ITEM3971" className="ladi-element">
-                  <Form.Item
-                    name="decision"
-                    rules={[
-                      {
-                        required: true,
-                        message: "",
-                      },
-                    ]}
-                  >
-                    <ConfigProvider
-                      theme={{
-                        token: {
-                          colorPrimary: "#00b96b",
+                    <Form.Item
+                      name="decision"
+                      rules={[
+                        {
+                          required: true,
+                          message: "",
                         },
-                      }}
+                      ]}
                     >
                       <Radio.Group>
                         <Radio checked value={1} className="text-white">
@@ -403,10 +396,10 @@ export const Home = () => {
                           Bận mất rồi
                         </Radio>
                       </Radio.Group>
-                    </ConfigProvider>
-                  </Form.Item>
+                    </Form.Item>
+                  </ConfigProvider>
                 </div>
-                <div id="FORM_ITEM398" className="ladi-element">
+                <div id="FORM_ITEM3970" className="ladi-element">
                   <Form.Item
                     name="people"
                     rules={[
@@ -424,25 +417,28 @@ export const Home = () => {
                     />
                   </Form.Item>
                 </div>
-                <div id="FORM_ITEM399" className="ladi-element">
-                  <ConfigProvider
-                    theme={{
-                      token: {
-                        colorPrimary: "#00b96b",
-                      },
-                    }}
-                  >
-                    <Button
-                      style={{
-                        backgroundColor: "#00b96b",
-                        borderColor: "#00b96b",
-                        color: "white",
-                        fontWeight: "bold",
+                <div id="FORM_ITEM3971" className="ladi-element">
+                  <Form.Item>
+                    <ConfigProvider
+                      theme={{
+                        token: {
+                          colorPrimary: "#00b96b",
+                        },
                       }}
                     >
-                      Xác nhận{" "}
-                    </Button>
-                  </ConfigProvider>
+                      <Button
+                        htmlType="submit"
+                        style={{
+                          backgroundColor: "#00b96b",
+                          borderColor: "#00b96b",
+                          color: "white",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Xác nhận{" "}
+                      </Button>
+                    </ConfigProvider>
+                  </Form.Item>
                 </div>
               </Form>
             </div>
